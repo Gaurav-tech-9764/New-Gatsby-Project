e8f1dd244ff606fe5200ac97ee53ca8d29e7426f
myusername <gaurav.nakhale@neosoftmail.com>
2021-12-02 14:20:58 +0530

    gatsby-project-2/12

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,73 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import {graphql, Link} from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import styled from "styled-components"
 
-const IndexPage = () => (
+const Title = styled.h1`
+  display: inline-block;
+`
+
+const BlogBody = styled.div`
+  margin-bottom: 50px;
+`
+
+const BlogLink =styled(Link)`
+text-decoration:none;
+`
+const Blogtitle = styled.h3`
+margin-botton:20px;
+color:blue;
+`
+
+export default  ({data}) => {
+  console.log("this is the Query Data",data)
+  return(
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <Title>Hi people</Title>
+    <p>Welcome to your new Gaurav Blog site.</p>
+   <div>
+  <span>   Blogs Posts -  {data.allMarkdownRemark.totalCount}</span>
+  <br/>
+  {
+    data.allMarkdownRemark.edges.map(({node})=>
+    (
+      <BlogBody key={node.id}>
+      <BlogLink to={node.fields.slug}>
+      <Blogtitle>{node.frontmatter.title} - {node.frontmatter.date}</Blogtitle>
+      </BlogLink>
+      
+        <p>{node.excerpt}</p>
+      </BlogBody>
+    ))
+  }
+  
+   </div>
+ 
   </Layout>
-)
+)}
 
-export default IndexPage
+
+export const query = graphql`
+query{
+  allMarkdownRemark(sort: {fields:[frontmatter___date], order: DESC}) {
+    totalCount
+    edges {
+      node {
+        id
+        frontmatter {
+          description
+          date
+          title
+        }
+        fields {
+          slug
+        }
+        excerpt
+      }
+    }
+  }
+}
+
+`
---
+++ src/pages/page-3.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+  <Layout>
+   
+    <h1>Hello Welcome To Third Page</h1>
+   
+  </Layout>
+)
+
+export default ThirdPage
